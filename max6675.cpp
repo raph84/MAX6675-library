32d0b3e81f1de62c2ef8cf157a12611582fb6bc4
raph84 <raph.berube@gmail.com>
2020-11-20 21:31:15 -0500

    use with esp32

--- max6675.cpp
+++ max6675.cpp
@@ -1,6 +1,7 @@
 // this library is public domain. enjoy!
 // https://learn.adafruit.com/thermocouple/
 
+#include <stdlib.h>
 #include "max6675.h"
 
 /**************************************************************************/
@@ -18,7 +19,7 @@ MAX6675::MAX6675(int8_t SCLK, int8_t CS, int8_t MISO) {
 
   // define pin modes
   pinMode(cs, OUTPUT);
-  pinMode(sclk, OUTPUT);
+  pinMode(sclk, OUTPUT); 
   pinMode(miso, INPUT);
 
   digitalWrite(cs, HIGH);
@@ -30,12 +31,12 @@ MAX6675::MAX6675(int8_t SCLK, int8_t CS, int8_t MISO) {
     @returns Temperature in C or NAN on failure!
 */
 /**************************************************************************/
-float MAX6675::readCelsius(void) {
+double MAX6675::readCelsius(void) {
 
   uint16_t v;
 
   digitalWrite(cs, LOW);
-  delayMicroseconds(10);
+  delay(1);
 
   v = spiread();
   v <<= 8;
@@ -45,7 +46,7 @@ float MAX6675::readCelsius(void) {
 
   if (v & 0x4) {
     // uh oh, no thermocouple attached!
-    return NAN;
+    return NAN; 
     // return -100;
   }
 
@@ -60,22 +61,24 @@ float MAX6675::readCelsius(void) {
     @returns Temperature in F or NAN on failure!
 */
 /**************************************************************************/
-float MAX6675::readFahrenheit(void) { return readCelsius() * 9.0 / 5.0 + 32; }
+double MAX6675::readFahrenheit(void) {
+  return readCelsius() * 9.0/5.0 + 32;
+}
 
-byte MAX6675::spiread(void) {
+byte MAX6675::spiread(void) { 
   int i;
   byte d = 0;
 
   for (i = 7; i >= 0; i--) {
     digitalWrite(sclk, LOW);
-    delayMicroseconds(10);
+    delay(1);
     if (digitalRead(miso)) {
       // set the bit to 0 no matter what
       d |= (1 << i);
     }
 
     digitalWrite(sclk, HIGH);
-    delayMicroseconds(10);
+    delay(1);
   }
 
   return d;
--- max6675.h
+++ max6675.h
@@ -1,10 +1,11 @@
 // this library is public domain. enjoy!
 // https://learn.adafruit.com/thermocouple/
 
-#ifndef ADAFRUIT_MAX6675_H
-#define ADAFRUIT_MAX6675_H
-
-#include "Arduino.h"
+#if ARDUINO >= 100
+ #include "Arduino.h"
+#else
+ #include "WProgram.h"
+#endif
 
 /**************************************************************************/
 /*!
@@ -12,19 +13,14 @@
 */
 /**************************************************************************/
 class MAX6675 {
-public:
+ public:
   MAX6675(int8_t SCLK, int8_t CS, int8_t MISO);
 
-  float readCelsius(void);
-  float readFahrenheit(void);
-
-  /*!    @brief  For compatibility with older versions
-         @returns Temperature in F or NAN on failure! */
-  float readFarenheit(void) { return readFahrenheit(); }
-
-private:
+  double readCelsius(void);
+  double readFahrenheit(void);
+  // For compatibility with older versions:
+  double readFarenheit(void) { return readFahrenheit(); }
+ private:
   int8_t sclk, miso, cs;
   uint8_t spiread(void);
 };
-
-#endif
